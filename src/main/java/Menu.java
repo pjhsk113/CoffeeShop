18aa8f73c33ce38b2b389968a80209a9035d15d4
pjhsk113 <pjhsk113@naver.com>
2021-04-08 22:59:22 +0900

    [refactor] Menu 클래스 Enum으로 변경

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,14 +1,37 @@
-import java.util.List;
+import java.util.Arrays;
 
-public class Menu {
-    List<MenuItem> menu;
+public enum Menu {
+    WATER(0, "물", 0),
+    AMERICANO(1, "아메리카노", 2500),
+    CAPPUCCINO(2, "카푸치노", 3000),
+    LATTE(3, "라떼", 3000);
 
-    private Menu(List<MenuItem> menu) {
-        this.menu = menu;
+    private final int menuNumber;
+    private final String coffeeName;
+    private final int price;
+
+    Menu(int menuNumber, String coffeeName, int price) {
+        this.menuNumber = menuNumber;
+        this.coffeeName = coffeeName;
+        this.price = price;
     }
 
-    public static Menu from(List<MenuItem> menu) {
-        return new Menu(menu);
+    public int getMenuNumber() {
+        return menuNumber;
     }
 
+    public int getPrice() {
+        return price;
+    }
+
+    public String getCoffeeName() {
+        return coffeeName;
+    }
+
+    public static Menu valueOf(int menuNumber) {
+        return Arrays.stream(values())
+                .filter(e -> e.getMenuNumber() == menuNumber)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("0,1,2,3 중에 입력 바람!"));
+    }
 }
