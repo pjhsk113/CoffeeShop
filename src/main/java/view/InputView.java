37d669bcbf0194159b4312bc92235973a5cb36f6
pjhsk113 <pjhsk113@naver.com>
2021-04-09 21:44:55 +0900

    [feat] InputView 추가

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,26 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private InputView() { }
+
+    private static final String MENU = "-----메뉴판-----\n 1: 아메리카노, 2: 카푸치노, 3: 라떼, 0: 물";
+    private static final String ENTER_MENU_MESSAGE = "메뉴 번호를 입력해주세요.";
+    private static final String ENTER_PAYMENT_MESSAGE = "금액을 입력해주세요.";
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void getMenu() {
+        System.out.println(MENU);
+    }
+
+    public static int enterMenuNumber() {
+        System.out.println(ENTER_MENU_MESSAGE);
+        return sc.nextInt();
+    }
+
+    public static int enterPayment() {
+        System.out.println(ENTER_PAYMENT_MESSAGE);
+        return sc.nextInt();
+    }
+}
