7fdad35b6e9d0f50c0aa5ebe4cd5c95359ad5577
pjhsk113 <pjhsk113@naver.com>
2021-04-09 21:44:22 +0900

    [feat] Cashier - 커피 값 계산

--- src/main/java/Cashier.java
+++ src/main/java/Cashier.java
@@ -1,2 +1,22 @@
 public class Cashier {
+    private static final String IS_NOT_ENOUGH_MONEY = "지불 금액이 부족합니다.";
+
+    public String requestPayMoney(int payment, int menuNumber) {
+        int coffeePrice = Menu.valueOf(menuNumber).getPrice();
+
+        if (payment < coffeePrice) {
+            validatePayAmount(payment, coffeePrice);
+        }
+        return requestMakeCoffee(menuNumber);
+    }
+
+    private String requestMakeCoffee(int menuNumber) {
+        return Barista.makeCoffee(menuNumber);
+    }
+
+    private void validatePayAmount(int payment, int coffeePrice ) {
+        if (payment < coffeePrice) {
+            throw new IllegalArgumentException(IS_NOT_ENOUGH_MONEY);
+        }
+    }
 }
