4dfb32dc58e7fad84f7c8ed14faeb5cdcf9de0c9
pjhsk113 <pjhsk113@naver.com>
2021-04-10 19:03:26 +0900

    [refactor] Barista static 메서드 변경

--- src/main/java/Barista.java
+++ src/main/java/Barista.java
@@ -1,7 +1,7 @@
 public class Barista {
-    private static final String FINISHED_MAKE_COFFEE = "%s가 완성되었습니다.\n";
+    private static final String FINISHED_MAKE_COFFEE = "%s(이)가 완성되었습니다.\n";
 
-    public static void makeCoffee(String orderedCoffee) {
+    public void makeCoffee(String orderedCoffee) {
         System.out.printf(FINISHED_MAKE_COFFEE, orderedCoffee);
     }
 }
