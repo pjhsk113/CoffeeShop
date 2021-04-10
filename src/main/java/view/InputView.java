8682b5afd53c00b014eec4489ed0844b96d3da96
pjhsk113 <pjhsk113@naver.com>
2021-04-10 19:02:20 +0900

    [refactor] Input - Menu 메시지에서 물 삭제

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InputView {
     private InputView() { }
 
-    private static final String MENU = "-----메뉴판-----\n 1: 아메리카노-2500원, 2: 카푸치노-3000원, 3: 라떼-3000원, 0: 물";
+    private static final String MENU = "-----메뉴판-----\n 1: 아메리카노-2500원, 2: 카푸치노-3000원, 3: 라떼-3000원";
     private static final String ENTER_MENU_MESSAGE = "메뉴 번호를 입력해주세요.";
     private static final String ENTER_PAYMENT_MESSAGE = "지불 할 금액을 입력해주세요.";
     private static final Scanner sc = new Scanner(System.in);
