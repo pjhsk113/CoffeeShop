a76e7a0ee61103a3d26be6ba527304f566975382
pjhsk113 <pjhsk113@naver.com>
2021-04-10 19:05:49 +0900

    [feat] 메인 애플리케이션 로직 추가

--- src/main/java/CoffeeShop.java
+++ src/main/java/CoffeeShop.java
@@ -1,2 +1,14 @@
+import view.InputView;
+
 public class CoffeeShop {
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+        Cashier cashier = new Cashier(new Barista());
+
+        InputView.getMenu();
+        int menuNumber = InputView.enterMenuNumber();
+        int payMoney = InputView.enterPayment();
+
+        customer.order(menuNumber, payMoney, cashier);
+    }
 }
